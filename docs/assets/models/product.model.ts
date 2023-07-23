1c318bf9070c37f18548287d6d36195d9d8a5742
Aena <annu.cuty@gmail.com>
2023-07-23 20:31:20 +0530

    publishing to github

---
+++ docs/assets/models/product.model.ts
@@ -0,0 +1,15 @@
+export class Product{
+    id:number = 0;
+    name:String = "";
+    imgsrc:string="";
+    price:number=0;
+    size:string[] = [];
+    flange:string[]=[];
+    dimensions:string[]=[];
+    lubrication:string[]=[];
+    fitting:string[]=[];
+    material:string[]=[];
+    description:string="";
+    features : string[] = [];
+    example : string[] = []
+}
